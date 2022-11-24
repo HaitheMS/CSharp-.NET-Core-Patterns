466140beb7def2c5ac9f9259a080c76f79f7cc54
Haïthem BEN AYOUB <instinctbuilding@gmail.com>
2022-11-24 22:41:34 +0100

    C# Patterns Directory initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# C#.NET-Core HBA Patterns
+
+This repository is built to make it easy the workflow with C# during your projects. You can use the commented use cases to fit your goals.
---
+++ premier_programme/premier_programme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace premier_programme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom_de_la_personne = "Maha"; // Affectation avec un égal =, Pour les chaines de caractères utiliser string
+            Console.WriteLine("Bonjour toto!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjour, je suis " + nom_de_la_personne + " HARRAZI!");
+            Console.WriteLine("Maha c'est bien mon prénom!");
+
+        }
+    }
+}
